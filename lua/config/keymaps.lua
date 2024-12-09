7ab6413c527599c25b9b2e505befaa6a43ce14a5
Ryan Megarity <ryan.megarity@immerse.io>
2024-12-09 12:14:25 +0000

    macros and extras

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map(
+  "n",
+  "lw",
+  'viwyA:<Esc>vbgsa(vwgsa"A<Left>,<Space><Esc>p^iconsole.log<Esc>',
+  { desc = "Add console log for word under cursor", remap = true }
+)
